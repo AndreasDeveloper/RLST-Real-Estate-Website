f9e2850ce3c0f62a238e99aa1356a391fbca3eec
AndreasDEV <andreasmrdja16@gmail.com>
2019-05-10 17:27:54 +0200

    Implement featured slider. Make services responsive. | Update #2 |

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,2 +1,5 @@
-// * --- Importing SASS --- * \\
+// * --- Importing SASS Files --- * \\
 import '../sass/main.scss';
+// * --- Importing JS Files --- * \\
+import * as sliderView from './views/slider';
+import * as baseViews from './views/base';
---
+++ resources/js/views/base.js
@@ -0,0 +1,11 @@
+// Getting DOM Elements
+export const DOMElements = {
+    slider: document.querySelector('.slider'),
+    sliderBg: document.querySelector('.slide__bg'),
+    sliderPagination: document.querySelector('.slider-pagi'),
+    sliderPaginationEl: document.querySelector('.slider-pagi__elem'),
+    slide: document.querySelector('.slide'),
+    sliderControl: document.querySelector('.slider-control'),
+    sliderControlLeft: document.querySelector('.slider-control__left'),
+    sliderControlRight: document.querySelector('.slider-control__right')
+};
