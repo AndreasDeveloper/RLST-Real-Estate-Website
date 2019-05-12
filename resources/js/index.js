615e7e1c304d7d0fef2bbd53a3aaecf53800ff83
AndreasDEV <andreasmrdja16@gmail.com>
2019-05-12 21:26:50 +0200

    Implement quich search and availability sections. Add 2 new drag sliders. | Update # |

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,4 +2,6 @@
 import '../sass/main.scss';
 // * --- Importing JS Files --- * \\
 import * as sliderView from './views/slider';
+import * as qsSliderView from './views/qs-slider';
+import * as avSliderView from './views/available-slider';
 import * as baseViews from './views/base';
---
+++ resources/js/views/available-slider.js
@@ -0,0 +1,72 @@
+/*global $, console*/
+
+// DOM Elements
+let slider = document.querySelector('.qs-slider-elements-2'),
+    arrows = document.querySelectorAll('.arrows .arr-left, .arrows .arr-right'),
+    isDown = false,
+    startX,
+    scrollLeft;
+slider.scrollLeft = 1970;
+
+
+slider.onmousedown = function (e) {
+    'use strict';
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+};
+
+slider.onmouseup = function () {
+    'use strict';
+    isDown = false;
+    slider.classList.remove('active');
+};
+
+slider.onmouseleave = function () {
+    'use strict';
+    isDown = false;
+    slider.classList.remove('active');
+};
+
+slider.onmousemove = function (e) {
+    'use strict';
+    if (!isDown) { return; }
+    e.preventDefault();
+    var x = e.pageX - slider.offsetLeft,
+        walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
+};
+
+function controlsSlider(num) {
+    'use strict';
+    var smooth = setInterval(function () {
+        slider.scrollLeft += num;
+    }, 10);
+    setTimeout(function () {
+        clearInterval(smooth);
+    }, 210);
+}
+arrows[0].onclick = function () {
+    'use strict';
+    controlsSlider(-10);
+};
+
+arrows[1].onclick = function () {
+    'use strict';
+    controlsSlider(10);
+};
+
+window.onkeydown = function (e) {
+    'use strict';
+    var key = e.keyCode;
+    if (key === 39) {
+        controlsSlider(10);
+    }
+    if (key === 37) {
+        controlsSlider(-10);
+    }
+};
+
+
+
